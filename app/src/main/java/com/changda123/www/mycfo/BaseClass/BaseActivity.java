4afa66a183a02320ed724a98d3a9f611c185ecce
flutist1225 <flutist1225@126.com>
2018-05-20 21:25:55 +0800

    change to MVP pattern(Not finish)

---
+++ app/src/main/java/com/changda123/www/mycfo/BaseClass/BaseActivity.java
@@ -0,0 +1,47 @@
+package com.changda123.www.mycfo.BaseClass;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.changda123.www.mycfo.R;
+
+public abstract class BaseActivity extends AppCompatActivity implements IBaseView {
+
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setCancelable(false);
+    }
+
+    @Override
+    public void showLoading() {
+        if (!mProgressDialog.isShowing()) {
+            mProgressDialog.show();
+        }
+    }
+    @Override
+    public void hideLoading() {
+        if (mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+    @Override
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void showErr() {
+        showToast(getResources().getString(R.string.api_error_msg));
+    }
+    @Override
+    public Context getContext() {
+        return BaseActivity.this;
+    }
+}
